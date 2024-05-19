2db8c41fe6334ff9ddcad8c73686c0483fb1181f
iamRazzakk <rakibt23p@gmail.com>
2024-05-19 00:07:41 -0400

    learning writing text use nodejs syntext use writeFileSync

--- index-2.js
+++ index-2.js
@@ -1,4 +1,8 @@
 const fs = require('fs')
 // reading a file syncly
 const readMyText = fs.readFileSync('./Text/mytext.txt', "utf-8")
+
 console.log(readMyText)
+// writing a text
+const writterText = fs.writeFileSync('./Text/myWritten.txt', "Hello world how are you?")
+console.log(writterText)
